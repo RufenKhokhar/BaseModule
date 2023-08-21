f8ee2ec68f181ebf478a313113ee1f6b2141deec
SarmadNazer <sarmadnazer324@gmail.com>
2023-08-21 12:45:32 +0500

    remove hilt

--- app/src/main/java/com/rkgroup/base/module/MainActivity.kt
+++ app/src/main/java/com/rkgroup/base/module/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.rkgroup.base.module
 
+import android.Manifest
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import com.rkgroup.base.basemodule.extensions.preferences
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import android.view.View
+import androidx.core.view.drawToBitmap
+import com.rkgroup.base.basemodule.activity.BaseActivity1
+import com.rkgroup.base.basemodule.extensions.disable
+import com.rkgroup.base.basemodule.extensions.enable
+import com.rkgroup.base.basemodule.extensions.gone
+import com.rkgroup.base.basemodule.extensions.inVisible
+import com.rkgroup.base.basemodule.extensions.permissionGranted
+import com.rkgroup.base.basemodule.extensions.resourcesManager
+import com.rkgroup.base.basemodule.extensions.shareTextData
+import com.rkgroup.base.basemodule.extensions.visible
+import com.rkgroup.base.basemodule.recyclerview.RecyclerBaseAdapter
+import com.rkgroup.base.module.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity1<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+
+
     }
 
 
     companion object {
         private const val TAG = "MainActivity"
     }
+}
+
+class RVAdaptrer:RecyclerBaseAdapter<ActivityMainBinding,Int>(ActivityMainBinding::inflate){
+
+    override fun onBindViewHolder(holder: BaseView<ActivityMainBinding>, position: Int) {
+        super.onBindViewHolder(holder, position)
+        val item = getItemAt(position)
+        holder.binding.tvHello.text = item.toString()
+    }
+
 }
